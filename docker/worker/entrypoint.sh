58dda93009128e72786da27467754646e57598fe
Vito <vslavilla@gmail.com>
2017-05-28 01:40:51 -0400

    start cron in worker entrypoint

--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1="\2"/g' > /etc/environment
-# service cron start
-
+crond -b -l8 -L /var/log/cron
 /usr/local/bin/node /app/build/server/jobs/consumers
