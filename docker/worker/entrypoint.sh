9e17ef4ddeba299004966e635642758dde4e7381
Vito <vito@vitosamson.com>
2017-07-04 12:12:57 -0400

    add worker job to backup mongodb to aws glacier

--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# export the env vars passed to the docker container so they're available to the cron scripts
 printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1="\2"/g' > /etc/environment
+
+# start cron
 crond -b -l8 -L /var/log/cron
+
+# start the queue consumer
 /usr/local/bin/node /app/build/server/jobs/consumers
